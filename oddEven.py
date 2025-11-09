3dbad14a3b73ee17d56108d55724af5ac39d6e7c
Jamir15 <238633277+Jamir15@users.noreply.github.com>
2025-11-09 17:38:43 +0800

    oddEven.py

--- oddEven.py
+++ oddEven.py
@@ -5,8 +5,9 @@ print('This code will determine if the integer is odd or even') #Display message
 num = int(input('Enter an integer: ')) #get user input
 
 #logic
-if num % 2 == 0: #if the number is divided to 2 and the remainder is 0
+if num % 2 == 0: #if the number is divided to 2 and the remainder is 0, it is an even number
     print(f'{num} is an even number.')#if it is zero, it is even
 
 else: #since it can only output two values, if it is not even, it must be odd
+
     print(f'{num} is an odd number.')#if it is not zero and their is a remainder, it is odd
