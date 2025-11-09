435356041693da89a01d609e94664ddee9ce5117
Jamir15 <238633277+Jamir15@users.noreply.github.com>
2025-11-09 17:44:43 +0800

    Add files via upload

---
+++ CelsiusFarenheit.py
@@ -0,0 +1,11 @@
+#celsius to farenheit
+
+print('This code will convert celsius to farenheit') #Display message to user
+
+#I'm using float here to allow decimal points
+celsius = float(input('Enter temperature in celsius: ')) #get user input
+
+#formula to convert celsius to farenheit, kinuha ko lang sa Google hahaha!
+farenheit = (celsius * 9/5) + 32
+
+print(celsius, 'is ', farenheit, 'in farenheit') #print result
